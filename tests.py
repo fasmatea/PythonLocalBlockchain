63eceec8896eadfa4bb7bd691d0bf29bee5840ce
flying-scorpio <tfranken@protonmail.com>
2019-11-07 16:04:47 +0100

    wrote tests for mempool

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 """All the test cases are listed here."""
 
 import unittest
+from random import randint
+import mempool
 from blockchain import Blockchain
 
 
@@ -37,6 +39,25 @@ class TestBlock(unittest.TestCase):
                     '0' * self.blockchain.proof_difficulty
                 )
             )
+
+
+class TestMempool(unittest.TestCase):
+    """TestCase for the mempool."""
+
+    NB_TRANSACTIONS = randint(0, 10 * 1000)
+
+    def setUp(self):
+        self.test_mempool = mempool.create_mempool(self.NB_TRANSACTIONS)
+
+    def test_mempool_type(self):
+        """The mempool should be a list."""
+
+        self.assertIs(type(self.test_mempool), list)
+
+    def test_mempool_size(self):
+        """The mempool should contain the correct number of transactions."""
+
+        self.assertEqual(len(self.test_mempool), self.NB_TRANSACTIONS)
 
 
 if __name__ == '__main__':
